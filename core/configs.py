91b0901a3e2493f79a26cc7e6800539b8b42a0d6
Jan Huus <jhuus1@gmail.com>
2023-07-04 14:09:20 -0400

    Small fix to configs

--- core/configs.py
+++ core/configs.py
@@ -1,6 +1,6 @@
 # Each configuration is a dataclass that sets the members it needs,
 # as in the Test001 example below. To add a config, add a new dataclass here
-# and then add it to the configs list and the set_config method at the bottom.
+# and then add it to the configs dictionary below so it has a name.
 # This approach supports typeahead and error-checking, which are very useful.
 
 from dataclasses import dataclass
@@ -14,12 +14,13 @@ class Test001(base_config.BaseConfig):
     def __init__(self):
         self.train.num_epochs = 2
 
-# list for checking command-line argument
-configs = ["base", "test001"]
+# map names to configurations
+configs = {"base": base_config.BaseConfig,
+           "test001": Test001}
 
 # set a configuration based on the name
 def set_config(name):
-    if name == "base":
-        cfg = base_config.BaseConfig()
-    elif name == "test001":
-        cfg = Test001()
+    if name in configs:
+        cfg = configs[name]()
+    else:
+        raise Exception(f"Configuration '{name}' not defined")
