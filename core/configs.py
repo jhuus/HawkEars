bcafddf39aba0219070b53f317bcbc9e95b7e022
Jan Huus <jhuus1@gmail.com>
2023-09-17 11:01:52 -0400

    add search config to configs.py

--- core/configs.py
+++ core/configs.py
@@ -29,6 +29,18 @@ class Low_Band(base_config.BaseConfig):
         self.train.num_epochs = 18
         self.train.save_last_n = 4
 
+# Single-label classifier for spectrogram searching and clustering
+@dataclass
+class Search(base_config.BaseConfig):
+    def __init__(self):
+        self.misc.train_pickle = "data/all-train.pickle"
+        self.misc.test_pickle = None
+        self.train.model_name = "mobilenetv3_large_100"
+        self.train.multi_label = False
+        self.train.deterministic = False
+        self.train.num_epochs = 1
+        self.train.save_last_n = 1
+
 # All tf_efficientnetv2_b0 from scratch
 @dataclass
 class All_Eff(base_config.BaseConfig):
@@ -43,6 +55,7 @@ class All_Eff(base_config.BaseConfig):
 # map names to configurations
 configs = {"base": base_config.BaseConfig,
            "low_band": Low_Band,
+           "search": Search,
            "all_eff": All_Eff}
 
 # set a configuration based on the name
