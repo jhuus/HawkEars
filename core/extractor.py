4869acad14799265cae16316897153e50be67d96
Jan Huus <jhuus1@gmail.com>
2023-09-18 13:09:16 -0400

    do not insert duplicate spectrograms

--- core/extractor.py
+++ core/extractor.py
@@ -12,7 +12,7 @@ from core import util
 class Extractor:
     def __init__(self, audio_path, db_name, source, category, species_name, species_code, low_band):
         self.audio_path = audio_path # path to folder containing recordings to extract from
-        self.db = self.get_database(db_name)
+        self.db = self._get_database(db_name)
         self.source = source
         self.category = category
         self.species_name = species_name
@@ -20,12 +20,13 @@ class Extractor:
         self.low_band = low_band # true only when extracting low-band spectrograms for Ruffed Grouse drumming detection
         self.audio = audio.Audio()
 
-        self.get_category_id()
-        self.get_subcategory_id()
+        self._get_category_id()
+        self._get_subcategory_id()
+        self._get_db_specs()
 
     # If db_name ends in ".db", treat it as a full path.
     # Otherwise use the path "../data/{db_name}.db"
-    def get_database(self, db_name):
+    def _get_database(self, db_name):
         if db_name.endswith('.db'):
             db = database.Database(db_name)
         else:
@@ -34,7 +35,7 @@ class Extractor:
         return db
 
     # category examples: 'bird', 'mammal', 'insect'...
-    def get_category_id(self):
+    def _get_category_id(self):
         results = self.db.get_category('Name', self.category)
         if len(results) == 0:
             self.category_id = self.db.insert_category(self.category)
@@ -42,6 +43,16 @@ class Extractor:
             self.category_id = results[0].id
 
         return self.category_id
+
+    # get existing spectrograms, so we don't insert duplicates
+    def _get_db_specs(self):
+        self.specs = {}
+        results = self.db.get_spectrogram_by_subcat_name(self.species_name, include_ignored=True)
+        for r in results:
+            if r.recording_id not in self.specs:
+                self.specs[r.recording_id] = {}
+
+            self.specs[r.recording_id][round(r.offset, 0)] = 1
 
     def get_recording_paths(self):
         return util.get_audio_files(self.audio_path)
@@ -61,7 +72,7 @@ class Extractor:
         return self.source_id
 
     # a subcategory is a specific class, e.g. "American Robin"
-    def get_subcategory_id(self):
+    def _get_subcategory_id(self):
         results = self.db.get_subcategory('Name', self.species_name)
         if len(results) == 0:
             self.subcategory_id = self.db.insert_subcategory(self.category_id, self.species_name, code=self.species_code)
@@ -90,10 +101,25 @@ class Extractor:
         source_id = self.get_source_id(filename)
         recording_id = self.get_recording_id(filename, source_id, seconds)
 
+        num_inserted = 0
         specs = self.audio.get_spectrograms(offsets, segment_len=cfg.audio.segment_len, low_band=self.low_band)
         for i in range(len(specs)):
+            # check for duplicate before inserting
+            if recording_id not in self.specs:
+                self.specs[recording_id] = {}
+
+            check_offset = round(offsets[i], 0)
+            if (check_offset in self.specs[recording_id]
+                or check_offset - 1 in self.specs[recording_id]
+                or check_offset + 1 in self.specs[recording_id]):
+                continue
+
+            num_inserted += 1
+            self.specs[recording_id][check_offset] = 1 # so we don't insert it again
             compressed = util.compress_spectrogram(specs[i])
             self.db.insert_spectrogram(recording_id, compressed, offsets[i])
+
+        return num_inserted
 
     def load_audio(self, recording_path):
         signal, rate = self.audio.load(recording_path)
