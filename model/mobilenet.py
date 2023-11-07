4ae94fc1e9202b27bd75a66eda2d3372cb5f7d06
Jan Huus <jhuus1@gmail.com>
2023-11-07 11:22:53 -0500

    reorganize custom models

--- model/mobilenet.py
+++ model/mobilenet.py
@@ -4,21 +4,33 @@ import torch
 import timm
 from timm.models import mobilenetv3
 
+# all models are mobilenetv3_large_100 with different channel multipliers
 def get_model(model_name, **kwargs):
     if model_name == '1':
-        # like mobilenetv3_large_100 but channel_multiplier = 1.25 instead of 1.0;
-        # ~6.3M parameters
-        model = mobilenetv3._gen_mobilenet_v3('mobilenetv3_large_100', 1.25, pretrained=False, in_chans=1, **kwargs)
+        # ~1.5M parameters
+        channel_multiplier = .5
     elif model_name == '2':
-        # like mobilenetv3_large_100 but channel_multiplier = 1.5 instead of 1.0
-        # ~8.5M parameters
-        model = mobilenetv3._gen_mobilenet_v3('mobilenetv3_large_100', 1.5, pretrained=False, in_chans=1, **kwargs)
+        # ~2.4M parameters
+        channel_multiplier = .7
     elif model_name == '3':
-        # like mobilenetv3_large_100 but channel_multiplier = 1.75 instead of 1.0
-        # ~11.3M parameters
-        model = mobilenetv3._gen_mobilenet_v3('mobilenetv3_large_100', 1.75, pretrained=False, in_chans=1, **kwargs)
+        # ~4.2M parameters
+        channel_multiplier = 1.0 # i.e. this is mobilenetv3_large_100
+    elif model_name == '4':
+        # ~5.0M parameters
+        channel_multiplier = 1.1
+    elif model_name == '6':
+        # ~6.3M parameters
+        channel_multiplier = 1.25
+    elif model_name == '7':
+        # ~7.2M parameters
+        channel_multiplier = 1.35
+    elif model_name == '8':
+        # ~8.5M parameters
+        channel_multiplier = 1.5
     else:
         raise Exception(f"Unknown custom DLA model name: {model_name}")
+
+    model = mobilenetv3._gen_mobilenet_v3('mobilenetv3_large_100', channel_multiplier, pretrained=False, in_chans=1, **kwargs)
 
     return model
 
