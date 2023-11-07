5c423715fe8b50b5ea641d9f58de1c97efea0c56
Jan Huus <jhuus1@gmail.com>
2023-11-07 11:18:22 -0500

    various config changes, especially higher max frequency

--- core/base_config.py
+++ core/base_config.py
@@ -8,11 +8,11 @@ class Audio:
     sampling_rate = 40960
     hop_length = 320
     win_length = 2048
-    spec_height = 128       # spectrogram height
+    spec_height = 192       # spectrogram height
     spec_width = 384        # spectrogram width (3 * 128)
     check_seconds = 5       # check prefix of this length when picking cleanest channel
-    min_audio_freq = 200
-    max_audio_freq = 10500
+    min_audio_freq = 200    # need this low for American Bittern
+    max_audio_freq = 13000  # need this high for Chestnut-backed Chickadee "seet series"
     mel_scale = True
     power = 1
     spec_block_seconds = 240 # max seconds of spectrogram to create at a time (limited by GPU memory)
@@ -37,9 +37,10 @@ class Training:
     use_class_weights = True
     load_ckpt_path = None   # for transfer learning or fine-tuning
     fine_tuning = False     # if true, don't freeze the feature extractor
-    dropout = None          # dropout, drop_rate and drop_path_rate are passed to model only if not None
+    dropout = None          # various dropout parameters are passed to model only if not None
     drop_rate = None
     drop_path_rate = None
+    proj_drop_rate = None
     num_epochs = 10
     save_last_n = 3         # save checkpoints for this many last epochs
     label_smoothing = 0.15
@@ -65,7 +66,7 @@ class Training:
 
 @dataclass
 class Inference:
-    min_prob = 0.75              # minimum confidence level
+    min_prob = 0.73              # minimum confidence level
     use_banding_codes = True     # use banding codes instead of species names in labels
     check_adjacent = True        # omit label unless adjacent segment matches
     adjacent_prob_factor = 0.65  # when checking if adjacent segment matches species, use self.min_prob times this
@@ -92,7 +93,7 @@ class Inference:
 class Miscellaneous:
     main_ckpt_folder = "data/ckpt"      # use an ensemble of all checkpoints in this folder for inference
     low_band_ckpt_path = "data/low_band.ckpt"
-    search_ckpt_path = "data/search.ckpt" # single-label model checkpoint used in searching and clustering
+    search_ckpt_path = "data/ckpt/custom_efficientnet_5.ckpt" # checkpoint used in searching and clustering
     classes_file = "data/classes.txt"   # list of classes used to generate pickle files
     ignore_file = "data/ignore.txt"     # classes listed in this file are ignored in analysis
     train_pickle = "data/ssw0-train.pickle"
@@ -103,6 +104,8 @@ class Miscellaneous:
     source_regexes = [
         ("XC\d+", "Xeno-Canto"),
         ("N\d+", "iNaturalist"),
+        ("W\d+", "Wildtrax"),
+        ("HNC.*", "HNC"),
         ("\d+", "Macaulay Library"),
         (".*", "Other")]
 
