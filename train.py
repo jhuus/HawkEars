e74c63326aaca4c8a4fccb0b18bc929ad0ca82fe
Jan Huus <jhuus1@gmail.com>
2024-04-09 11:21:06 -0400

    fix retraining logic (retrained models gave error on load)

--- train.py
+++ train.py
@@ -43,22 +43,9 @@ class Trainer:
 
             # create model inside loop so parameters are reset for each fold,
             # and so metrics are tracked correctly
-            if cfg.train.load_ckpt_path is not None:
-                logging.info(f"Loading model from {cfg.train.load_ckpt_path}")
-                model = main_model.MainModel.load_from_checkpoint(cfg.train.load_ckpt_path)
-                if cfg.train.freeze_backbone:
-                    model.freeze()
-
-                if cfg.train.update_classifier:
-                    model.update_classifier(dm.train_class_names, dm.train_class_codes, dm.test_class_names, weights)
-                else:
-                    model.unfreeze_classifier()
-            else:
-                model = main_model.MainModel(dm.train_class_names, dm.train_class_codes, dm.test_class_names, weights,
-                                             cfg.train.model_name, cfg.train.load_weights, dm.num_train_specs)
+            model = main_model.MainModel(dm.train_class_names, dm.train_class_codes, dm.test_class_names, weights, cfg.train.model_name, cfg.train.load_weights)
 
             if cfg.train.compile:
-                # skip compile for short tests
                 model = torch.compile(model)
 
             trainer.fit(model, dm)
