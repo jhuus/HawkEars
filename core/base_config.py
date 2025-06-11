2d3880c507de1ff09861d72799e2671ceb0fa7f7
Jan Huus <jhuus1@gmail.com>
2025-06-11 10:26:04 -0400

    update low-band ckpt

--- core/base_config.py
+++ core/base_config.py
@@ -6,28 +6,27 @@ from dataclasses import dataclass
 class Audio:
     # sampling rate should be a multiple of spec_width / segment_len,
     # so that hop length formula gives an integer (segment_len * sampling_rate / spec_width)
-    segment_len = 3         # spectrogram duration in seconds
 
     spec_height = 192       # spectrogram height
     spec_width = 384        # spectrogram width (3 * 128)
-    sampling_rate = 37120
     win_length = 2048
     min_audio_freq = 200    # need this low for American Bittern
     max_audio_freq = 13000  # need this high for Chestnut-backed Chickadee "seet series"
+    mel_scale = True
 
+    segment_len = 3         # spectrogram duration in seconds
+    sampling_rate = 37120
     choose_channel = True   # use heuristic to pick the cleanest audio channel
     check_seconds = 3        # check segment of this length when picking cleanest channel
-    mel_scale = True
     power = 1.0
     spec_block_seconds = 240 # max seconds of spectrogram to create at a time (limited by GPU memory)
 
     # low-frequency audio settings for Ruffed Grouse drumming identifier
     low_band_spec_height = 64
-    low_band_min_audio_freq = 0
+    low_band_win_length = 4096
+    low_band_min_audio_freq = 30
     low_band_max_audio_freq = 200
-    low_band_win_length = 8192
     low_band_mel_scale = False
-    drop_bottom_freq = 6 # drop this many bottom frequencies in linear spectrograms
 
 @dataclass
 class Training:
@@ -75,6 +74,10 @@ class Training:
     min_fade2 = .1
     max_fade2 = 1
     speckle_variance = .012
+
+    # experimental feature; restrict to one test species for now
+    prob_attenuate = .1
+    attenuate_species = {"Willow Ptarmigan"}
 
     classic_mixup = False # classic mixup is implemented in main_model.py
     classic_mixup_alpha = 1.0
