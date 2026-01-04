8f1bb4cb1ef5c2e638f74932e152753964a691b7
Jan Huus <jhuus1@gmail.com>
2026-01-04 09:24:30 -0500

    support multiple soundalikes per species

--- src/hawkears/heuristics/canada/soundalike.py
+++ src/hawkears/heuristics/canada/soundalike.py
@@ -38,36 +38,38 @@ class SoundAlikeHeuristics:
         self.max_independent = 24  # treat as valid if >= this many independent frames
         self.min_coverage = 0.5  # treat as soundalike if it overlaps by this much
         self.no_location = {
-            "BWHA": SN(soundalike="WTSP", enabled=True),
-            "HASP": SN(soundalike="WTSP", enabled=True),
-            "VATH": SN(soundalike="WTSP", enabled=True),
+            "BWHA": [SN(soundalike="WTSP", enabled=True)],
+            "HASP": [SN(soundalike="WTSP", enabled=True)],
+            "VATH": [SN(soundalike="WTSP", enabled=True)],
         }
 
         # location-dependent cases
         self.max_rare = 0.001  # main species occurrence < this?
         self.min_common = 0.01  # soundalike species occurrence > this?
         self.with_location = {
-            "BOOW": SN(soundalike="WISN", enabled=True),
-            "CBCH": SN(soundalike="BCCH", enabled=True),
-            "EATO": SN(soundalike="SPTO", enabled=True),
-            "INBU": SN(soundalike="LAZB", enabled=True),
-            "LAZB": SN(soundalike="INBU", enabled=True),
-            "MOCH": SN(soundalike="BCCH", enabled=True),
-            "NOPO": SN(soundalike="CORA", enabled=True),
-            "RBSA": SN(soundalike="RNSA", enabled=True),
-            "RNSA": SN(soundalike="RBSA", enabled=True),
-            "SCTA": SN(soundalike="WETA", enabled=True),
-            "SPTO": SN(soundalike="EATO", enabled=True),
-            "WETA": SN(soundalike="SCTA", enabled=True),
-            "YBSA": SN(soundalike="RBSA", enabled=True),
-            "YTWA": SN(soundalike="SWSP", enabled=True),
+            "BOOW": [SN(soundalike="WISN", enabled=True)],
+            "CBCH": [SN(soundalike="BCCH", enabled=True)],
+            "EATO": [SN(soundalike="SPTO", enabled=True)],
+            "INBU": [SN(soundalike="LAZB", enabled=True)],
+            "LAZB": [SN(soundalike="INBU", enabled=True)],
+            "MOCH": [SN(soundalike="BCCH", enabled=True)],
+            "NOPO": [SN(soundalike="CORA", enabled=True)],
+            "RBSA": [SN(soundalike="RNSA", enabled=True), SN(soundalike="YBSA", enabled=True)],
+            "RNSA": [SN(soundalike="RBSA", enabled=True), SN(soundalike="YBSA", enabled=True)],
+            "SCTA": [SN(soundalike="WETA", enabled=True)],
+            "SPTO": [SN(soundalike="EATO", enabled=True)],
+            "WETA": [SN(soundalike="SCTA", enabled=True)],
+            "YBSA": [SN(soundalike="RBSA", enabled=True), SN(soundalike="RNSA", enabled=True)],
+            "YTWA": [SN(soundalike="SWSP", enabled=True)],
         }
 
-        for code, defn in self.no_location.items():
-            self._check_definition(code, defn)
+        for code, defns in self.no_location.items():
+            for defn in defns:
+                self._check_definition(code, defn)
 
-        for code, defn in self.with_location.items():
-            self._check_definition(code, defn)
+        for code, defns in self.with_location.items():
+            for defn in defns:
+                self._check_definition(code, defn)
 
     def __call__(
         self,
@@ -101,48 +103,51 @@ class SoundAlikeHeuristics:
         If high-scoring WTSP frames overlap sufficiently with high-scoring BWHA frames, set all BWHA scores
         to zero.
         """
-        for code, defn in self.no_location.items():
-            if not defn.enabled:
-                continue
+        for code, defns in self.no_location.items():
+            updated = False
+            for defn in defns:
+                if updated or not defn.enabled:
+                    continue
 
-            info = self.class_mgr.class_info_by_code(code)
-            if not info.include:
-                continue  # omitted from output anyway
+                info = self.class_mgr.class_info_by_code(code)
+                if not info.include:
+                    continue  # omitted from output anyway
 
-            class_idx = info.index
-            class_scores = frame_map[:, class_idx]
-            if (
-                class_scores.max() < self.cfg.infer.min_score
-                or class_scores.max() < self.min_score
-            ):
-                continue  # nothing to do
-            soundalike_idx = self.class_mgr.class_info_by_code(defn.soundalike).index
-            soundalike_scores = frame_map[:, soundalike_idx]
-            if soundalike_scores.max() < self.min_score:
-                continue  # nothing to do
+                class_idx = info.index
+                class_scores = frame_map[:, class_idx]
+                if (
+                    class_scores.max() < self.cfg.infer.min_score
+                    or class_scores.max() < self.min_score
+                ):
+                    continue  # nothing to do
+                soundalike_idx = self.class_mgr.class_info_by_code(defn.soundalike).index
+                soundalike_scores = frame_map[:, soundalike_idx]
+                if soundalike_scores.max() < self.min_score:
+                    continue  # nothing to do
 
-            # Spread high soundalike scores to adjacent frames
-            soundalike_scores = maximum_filter1d(
-                soundalike_scores, size=2 * self.frame_distance + 1, mode="constant"
-            )
+                # Spread high soundalike scores to adjacent frames
+                soundalike_scores = maximum_filter1d(
+                    soundalike_scores, size=2 * self.frame_distance + 1, mode="constant"
+                )
 
-            # Skip if enough high scores where there is no soundalike
-            class_mask = class_scores >= self.min_score
-            soundalike_mask = soundalike_scores >= self.min_score
-            independent = class_mask & ~soundalike_mask
-            if independent.sum() >= self.max_independent:
-                continue
+                # Skip if enough high scores where there is no soundalike
+                class_mask = class_scores >= self.min_score
+                soundalike_mask = soundalike_scores >= self.min_score
+                independent = class_mask & ~soundalike_mask
+                if independent.sum() >= self.max_independent:
+                    continue
 
-            # Calculate proportion of high class frames that overlap with high soundalike frames
-            covered = class_mask & soundalike_mask
-            class_sum = class_mask.sum()
-            if class_sum == 0:
-                continue  # avoid divide-by-zero
-            coverage = covered.sum() / class_sum
+                # Calculate proportion of high class frames that overlap with high soundalike frames
+                covered = class_mask & soundalike_mask
+                class_sum = class_mask.sum()
+                if class_sum == 0:
+                    continue  # avoid divide-by-zero
+                coverage = covered.sum() / class_sum
 
-            # If soundalike is sufficiently overlapping, zero out the class scores
-            if coverage >= self.min_coverage:
-                frame_map[:, class_idx] = 0.0
+                # If soundalike is sufficiently overlapping, zero out the class scores
+                if coverage >= self.min_coverage:
+                    frame_map[:, class_idx] = 0.0
+                    updated = True
 
     def _process_location_dependent(self, frame_map, recording_path):
         """
@@ -159,25 +164,28 @@ class SoundAlikeHeuristics:
             return  # location/date processing is disabled
 
         filename = Path(recording_path).name
-        for code, defn in self.with_location.items():
-            if not defn.enabled:
-                continue
+        for code, defns in self.with_location.items():
+            updated = False
+            for defn in defns:
+                if updated or not defn.enabled:
+                    continue
 
-            info = self.class_mgr.class_info_by_code(code)
-            if not info.include:
-                continue  # omitted from output anyway
+                info = self.class_mgr.class_info_by_code(code)
+                if not info.include:
+                    continue  # omitted from output anyway
 
-            other_info = self.class_mgr.class_info_by_code(defn.soundalike)
-            value = self.occur_mgr.get_value(filename, info.name)
-            other_value = self.occur_mgr.get_value(filename, other_info.name)
+                other_info = self.class_mgr.class_info_by_code(defn.soundalike)
+                value = self.occur_mgr.get_value(filename, info.name)
+                other_value = self.occur_mgr.get_value(filename, other_info.name)
 
-            if value < self.max_rare and other_value > self.min_common:
-                # The defined species is rare and the soundalike is common,
-                # so set soundalike scores to max of the two, then set defined
-                # species scores to zero.
-                from_idx = info.index
-                to_idx = other_info.index
-                frame_map[:, to_idx] = np.maximum(
-                    frame_map[:, to_idx], frame_map[:, from_idx]
-                )
-                frame_map[:, from_idx] = 0
+                if value < self.max_rare and other_value > self.min_common:
+                    # The defined species is rare and the soundalike is common,
+                    # so set soundalike scores to max of the two, then set defined
+                    # species scores to zero.
+                    from_idx = info.index
+                    to_idx = other_info.index
+                    frame_map[:, to_idx] = np.maximum(
+                        frame_map[:, to_idx], frame_map[:, from_idx]
+                    )
+                    frame_map[:, from_idx] = 0
+                    updated = True
