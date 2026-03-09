06cf2bcdf782514dffa9c30d5faa6dde667e8f55
Jan Huus <jhuus1@gmail.com>
2026-03-09 09:37:34 -0400

    add get_config to __init__.py for API

--- src/hawkears/__init__.py
+++ src/hawkears/__init__.py
@@ -20,8 +20,10 @@ except Exception:
         __version__ = "0.0.0"
 
 from hawkears import commands
+from hawkears.core.config_loader import get_config
 
 __all__ = [
     "__version__",
     "commands",
+    "get_config",
 ]
