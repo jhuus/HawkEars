25b27217039538141f009e0b9e662da9a81b0bad
Jan Huus <jhuus1@gmail.com>
2026-05-28 09:35:36 -0400

    update config to point to 2.1.0 model versions

--- src/hawkears/core/config.py
+++ src/hawkears/core/config.py
@@ -46,7 +46,7 @@ class HawkEarsConfig:
 
     low_band_classifier: bool = True  # if true, include low-band classifier
 
-    main_models_url = "https://github.com/jhuus/HawkEars/releases/download/models-2.0.0/main-models-2.0.0.zip"
+    main_models_url = "https://github.com/jhuus/HawkEars/releases/download/models-2.1.0/main-models-2.1.0.zip"
     low_band_models_url = "https://github.com/jhuus/HawkEars/releases/download/models-2.0.0/low-band-models-2.0.0.zip"
 
 
